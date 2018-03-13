17a7fedcb0c4c98399d78eb3803c90538f87ff95
Xabin Rodriguez <xabin95@gmail.com>
2018-03-13 12:43:11 +0100

    Initial Commit

---
+++ SwiftTutorial/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftTutorial
+//
+//  Created by Xabin Rodriguez on 13/3/18.
+//  Copyright © 2018 Xabin Rodriguez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
