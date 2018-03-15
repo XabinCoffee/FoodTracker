518059cbebab6fbea26701edaafcb433588478ee
Xabin Rodriguez <xabin95@gmail.com>
2018-03-15 14:40:27 +0100

    First Commit

---
+++ SwiftTutorial/MealTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  MealTableViewCell.swift
+//  SwiftTutorial
+//
+//  Created by Xabin Rodriguez on 15/3/18.
+//  Copyright © 2018 Xabin Rodriguez. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    //MARK: Properties
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var ratingControl: RatingControl!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+
+}
